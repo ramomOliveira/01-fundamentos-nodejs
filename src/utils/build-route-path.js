1f18e20d3648d303485d623e252e8880580fe0d0
Ramome <ra-mom2000@hotmail.com>
2024-04-01 20:12:45 -0300

    routes with passed parameters

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,9 +1,14 @@
 //regex e uma expressão regular (é uma forma de encontrar texto especifico dentro de um texto maior)
 
 export function buildRoutePath(path) {
+
+  // pagar o nome do parâmetro
   const routeParametersRegex = /:([a-zA-Z]+)/g
+  // ?<$1> sera o nome do parâmetro dentro do group
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-  console.log(Array.from(path.matchAll(routeParametersRegex)))
+  // "^" começa a partir do caractere inicial
+  const pathRegex = new RegExp(`^${pathWithParams}`)
 
-  // return new RegExp()
+  return pathRegex
 }
